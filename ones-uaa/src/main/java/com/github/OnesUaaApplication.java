4943f09f8cf41edf3da11c401da603b2ee70218e
许大仙 <1900919313@qq.com>
2022-07-17 20:35:59 +0800

    项目初始化

---
+++ ones-uaa/src/main/java/com/github/OnesUaaApplication.java
@@ -0,0 +1,18 @@
+package com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 许大仙
+ * @version 1.0
+ * @since 2022-07-14 14:35:03
+ */
+@SpringBootApplication
+public class OnesUaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnesUaaApplication.class, args);
+    }
+
+}
---
+++ ones-uaa/src/test/java/com/github/OnesUaaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnesUaaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
